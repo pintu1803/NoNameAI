d66a4a3cbd42d86dda0fbc8ecfae19ca1c00baba
Pintu Saini <pintusaininch2979@gmail.com>
2026-08-26 23:56:51 +0530

    image type

--- models/08_cnn.py
+++ models/08_cnn.py
@@ -1,6 +1,15 @@
 
 import torch
 import torchvision.datasets as datasets
+from torchvision import transforms
 
 
-d = datasets.CIFAR10(root="./deep_models/cifar_cnn/dataset", download=True, train=True)
+train = datasets.CIFAR10(root="./deep_models/cifar_cnn/dataset", download=True, train=True)
+
+img, label = train[0]
+
+print("Type of img : ",type(img))
+print("Size of img : ", img.size)
+
+# transform = transforms.ToTensor()
+# train = datasets.CIFAR10(root="./deep_models/cifar_cnn/dataset", download=False, train=True)
