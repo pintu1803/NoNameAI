20a05debf5706cc3a6649c9e2c47ce3dacaa4ce1
Pintu Saini <pintusaininch2979@gmail.com>
2026-08-27 00:00:14 +0530

    comment added

--- models/08_cnn.py
+++ models/08_cnn.py
@@ -3,6 +3,25 @@ import torch
 import torchvision.datasets as datasets
 from torchvision import transforms
 
+"""
+Images are tensors
+RGB = 3 channels
+Tensor shape = (N, C, H, W)
+N = batch size, C = channel, H = height, W = width
+Flattening destroys spatial relationships
+Convolution uses a small kernel to detect patterns
+Edge detection example
+
+---------------
+Image
+↓
+One Filter
+↓
+One Output Image
+
+That output image is called a Feature Map.
+---------------
+"""
 
 train = datasets.CIFAR10(root="./deep_models/cifar_cnn/dataset", download=True, train=True)
 
@@ -13,3 +32,5 @@ print("Size of img : ", img.size)
 
 # transform = transforms.ToTensor()
 # train = datasets.CIFAR10(root="./deep_models/cifar_cnn/dataset", download=False, train=True)
+
+
