bb871ff22e88d58c7f6622d2a08ae9e22fa2060a
Pintu Saini <pintusaininch2979@gmail.com>
2026-09-06 13:36:25 +0530

    add more configs

--- deep_models/resnet18/config.py
+++ deep_models/resnet18/config.py
@@ -12,20 +12,27 @@ print("Project Base dir : ", BASE_DIR)
 @dataclass
 class PATH:
     CHECKPOINT_DIR: str = BASE_DIR / "checkpoints"
+    CHECKPOINT_PATH: str = CHECKPOINT_DIR / "best_val_acc_model.pth"
     DOWNLOADE_MODEL_PATH: str = BASE_DIR / "model"
 
     DATASET_DIR: str = BASE_DIR / "dataset"
     PLOT_DIR: str = BASE_DIR / "plots"
 
+    LOSS_PLOT_NAME: str = PLOT_DIR / ""
+    ACC_PLOT_NAME: str = PLOT_DIR / ""
+
 @dataclass
 class IMAGE:
     height: int = 224
     width: int = 224
+    classes: int = 15
 
 @dataclass
 class TrainConfig:
-    train_percent = 0.85
-    valid_percent = 0.15
-    epochs_count = 20
-    batch_size = 32
-    test_batch_size = 16
+    train_percent: float = 0.85
+    valid_percent: float = 0.15
+    epochs_count: int = 20
+    batch_size: int = 32
+    test_batch_size: int = 16
+    lr: float = 0.001
+    decay: float = 0.002
